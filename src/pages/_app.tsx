cb511f24b63a0ab51b0cd22cbca5c2d81f88d823
Lucas Rimondi <lucas@flixxo.com>
2023-06-16 14:27:02 -0300

    Refactor to keep clean code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from "next/app";
-import Layout from "@/components/Layout";
-import "@/styles/globals.css";
 import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
