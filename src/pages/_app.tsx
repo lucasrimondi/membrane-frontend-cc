00e262794662acfcff276f06dc215c68a55e91b0
Lucas Rimondi <lucas@flixxo.com>
2023-06-16 14:25:16 -0300

    add global loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const loader = document.getElementById("globalLoader");
+      if (loader) loader.remove();
+    }
+  }, []);
+
   return (
     <>
       <Layout>
