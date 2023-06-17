8fa818f24f4cdd0adca49c71117e55f20ecdef01
Lucas Rimondi <lucas@flixxo.com>
2023-06-17 10:04:12 -0300

    Web3 initial configs, add specific libs (wagmi, web3modal)  and env variable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,31 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 
+//web3 configs
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { goerli } from "wagmi/chains";
+
+// 1. Get projectID at https://cloud.walletconnect.com
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
+
+// 2. Configure wagmi client
+const chains = [goerli];
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  publicClient,
+});
+
+// 3. Configure modal ethereum client
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -11,11 +36,15 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, []);
 
+  // 4. Wrap your app with WagmiProvider and add <Web3Modal /> component
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <WagmiConfig config={wagmiConfig}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
   );
 }
